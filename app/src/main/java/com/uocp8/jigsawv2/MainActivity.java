3f3a919eb9636a7b7d6042ba53e589b14c6cf89d
javentinuoc <byow@uoc.edu>
2021-11-20 13:26:23 +0100

    new branch audios

--- app/src/main/java/com/uocp8/jigsawv2/MainActivity.java
+++ app/src/main/java/com/uocp8/jigsawv2/MainActivity.java
@@ -3,10 +3,14 @@ package com.uocp8.jigsawv2;
 
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,8 +25,10 @@ import com.uocp8.jigsawv2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    MediaPlayer mp;
     private ActivityMainBinding binding;
+
+
 
 
 
@@ -43,8 +49,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
     }
+
+    mp = MediaPlayer.create(this, R.raw.sound_long);
+    mp.start();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

1642dc30a70c2ad4ce6ba9acd04773ff9811b99b
Diego <dromeros@uoc.edu>
2021-11-27 21:01:21 +0100

    Fix overall design details of ConstraintLayout

--- app/src/main/java/com/uocp8/jigsawv2/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/uocp8/jigsawv2/ui/dashboard/DashboardFragment.java
@@ -76,13 +76,13 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
+        /*final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
 
         return root;
     }

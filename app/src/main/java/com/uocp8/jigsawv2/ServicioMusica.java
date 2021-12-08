5ad3c83a91588c975fcc8a177b1b4328da5c757a
javentinuoc <byow@uoc.edu>
2021-12-08 18:21:31 +0100

    actualizacion con servidor musica

---
+++ app/src/main/java/com/uocp8/jigsawv2/ServicioMusica.java
@@ -0,0 +1,56 @@
+package com.uocp8.jigsawv2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ServicioMusica extends Service {
+
+    MediaPlayer miReproductor;
+
+    public void onCreate () {
+
+        super.onCreate();
+
+        miReproductor = MediaPlayer.create(this, R.raw.sound_long);
+
+        miReproductor.setLooping(true);
+
+        miReproductor.setVolume(100, 100);
+
+
+
+    }
+
+
+    public int onStartCommand (Intent intent, int flags, int startId) {
+
+        miReproductor.start();
+
+        return START_STICKY;
+
+
+    }
+
+    public void onDestroy() {
+
+        super.onDestroy();
+
+        if(miReproductor.isPlaying()) miReproductor.stop();
+
+        miReproductor.release();
+
+        miReproductor=null;
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
